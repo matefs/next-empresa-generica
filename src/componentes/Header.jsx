76a085e6b9e1b505022681c97e7c6bd96b82d24d
Mateus Schverz <mate_fs@hotmail.com>
2023-03-21 22:46:07 -0300

    Ajeitado identação

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -117,7 +117,7 @@ export default function SearchAppBar() {
             component="div"
             sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
           >
-           Sistema
+            Sistema
           </Typography>
           <Search>
             <SearchIconWrapper>

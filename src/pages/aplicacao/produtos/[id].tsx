e59272cb435ecf2b84881fa9f71f41a5d54a9db8
Mateus Schverz <mate_fs@hotmail.com>
2023-04-19 15:37:01 -0300

    informações do produto passada para dentro do componente card

--- src/pages/aplicacao/produtos/[id].tsx
+++ src/pages/aplicacao/produtos/[id].tsx
@@ -4,6 +4,12 @@ import { useState } from 'react';
 import axios from 'axios'
 import { useRouter } from 'next/router'
 import CircularProgress from '@mui/material/CircularProgress';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 
 function MyComponent() { 
@@ -23,9 +29,29 @@ function MyComponent() {
 
   return (
     <div>
-      <h1>Hello </h1>
-      
-      { Object.keys(produtoIndividual).length > 1  ? JSON.stringify(produtoIndividual) :
+
+ 
+      { Object.keys(produtoIndividual).length > 1  ? 
+              <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {produtoIndividual.nome}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card> :
         <CircularProgress />
        }
       

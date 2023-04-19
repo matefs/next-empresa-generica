f107dcc76c8dc6252fcfd74c28173743d18f1215
Mateus Schverz <mate_fs@hotmail.com>
2023-04-19 12:25:17 -0300

    Ajustado erro consultando produto individual

--- src/pages/aplicacao/produtos/[id].tsx
+++ src/pages/aplicacao/produtos/[id].tsx
@@ -1,12 +1,34 @@
  
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import axios from 'axios'
+import { useRouter } from 'next/router'
 
 
 function MyComponent() { 
+  const router = useRouter()
+  const { id }  = router.query 
+
+  const [ produtoIndividual, setProdutoIndividual] = useState({})
+
+  
+  useEffect(()=> { 
+    id != undefined ?  axios.get(`https://generic-api-backend.mateusschverz.repl.co/produtos/${id}`)
+     .then(respostaItem => setProdutoIndividual(respostaItem.data)) 
+     .catch( (e) => alert("Ocorreu um erro \n" + e))
+     : undefined 
+  
+    }, [id])
+
   return (
     <div>
       <h1>Hello </h1>
-      <p>This is a new React component </p>
+      
+      { Object.keys(produtoIndividual).length > 1  ? JSON.stringify(produtoIndividual) :
+       "Carergando..." 
+       }
+      
+
     </div>
   );
 }

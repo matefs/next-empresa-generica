27cf341dc55a2e652c1dfe1f67b63a9d2a0d76b0
Mateus Schverz <mate_fs@hotmail.com>
2023-04-19 00:15:05 -0300

    Criado rota para produto individual

---
+++ src/pages/aplicacao/produtos/[id].tsx
@@ -0,0 +1,15 @@
+ 
+import React from 'react';
+
+
+function MyComponent() { 
+  return (
+    <div>
+      <h1>Hello </h1>
+      <p>This is a new React component </p>
+    </div>
+  );
+}
+ 
+
+export default MyComponent;

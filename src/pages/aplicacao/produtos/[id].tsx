1cab288bab4a681699b79c7bc0460d8a97b1d25d
Mateus Schverz <mate_fs@hotmail.com>
2023-04-20 00:37:33 -0300

    Implementado typescript na variavel produtoIndividual

--- src/pages/aplicacao/produtos/[id].tsx
+++ src/pages/aplicacao/produtos/[id].tsx
@@ -10,13 +10,17 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Grid } from '@mui/material';
 
 
-function MyComponent() { 
+import { Produto } from '../types/types'
+
+
+function ProdutoIndividualComponente() { 
   const router = useRouter()
   const { id }  = router.query 
 
-  const [ produtoIndividual, setProdutoIndividual] = useState({})
+ const [produtoIndividual, setProdutoIndividual] = useState<Produto>({ id: 0, nome: '', preco: 0 });
 
   
   useEffect(() => { 
@@ -24,15 +28,24 @@ function MyComponent() {
      .then(respostaItem => setProdutoIndividual(respostaItem.data)) 
      .catch( (e) => alert("Ocorreu um erro \n" + e))
      : undefined 
-  
+ 
     }, [id])
 
   return (
     <div>
+<Grid
+  container
+  spacing={0}
+  direction="column"
+  alignItems="center"
+  justifyContent="center"
+  style={{ minHeight: '100vh', backgroundColor: '#222f3e'}}
+>
 
  
       { Object.keys(produtoIndividual).length > 1  ? 
-              <Card sx={{ maxWidth: 345 }}>
+
+<Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 140 }}
         image="/static/images/cards/contemplative-reptile.jpg"
@@ -40,25 +53,30 @@ function MyComponent() {
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {produtoIndividual.nome}
+          {produtoIndividual.nome} 
         </Typography>
+
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          Id: {produtoIndividual.id}
         </Typography>
+
+        <Typography variant="body2" color="text.secondary">
+          R$ {produtoIndividual.preco}  
+        </Typography>
+        
+
       </CardContent>
       <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+        <Button size="small">Editar</Button>
+        <Button size="small">Salvar</Button>
       </CardActions>
-    </Card> :
-        <CircularProgress />
-       }
+</Card> : <CircularProgress /> }
       
+      </Grid>
 
     </div>
   );
 }
  
 
-export default MyComponent;
+export default ProdutoIndividualComponente;

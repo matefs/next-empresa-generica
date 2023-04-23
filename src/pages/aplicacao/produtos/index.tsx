c1691839d35a27f2ec2a49d851f8fb7655578907
Mateus Schverz <mate_fs@hotmail.com>
2023-04-23 14:32:12 -0300

    add: card adicionar novo produot

--- src/pages/aplicacao/produtos/index.tsx
+++ src/pages/aplicacao/produtos/index.tsx
@@ -49,6 +49,30 @@ const Produtos = () => {
         </Box>
 
 
+
+<Card sx={{  width:300, maxWidth: 745 }}>
+
+      <CardMedia
+        sx={{ height: 260, maxHeight: 700 }}
+        image={'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F582%2F754%2Foriginal%2Fvector-button-plus-icon.jpg&f=1&nofb=1&ipt=24249235270c265685bda77c0dfe13015c81fcc2f83370cc4e29dc210d2636dd&ipo=images'}
+      />
+
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Adicionar novo produto 
+        </Typography>
+
+        <Typography variant="body2" color="text.secondary">
+          Clique aqui para adicionar um novo produto
+        </Typography>
+ 
+
+      </CardContent>
+   
+
+</Card> 
+
+
       { listaProdutos.length < 1 ? 
       <CircularProgress style={{width: '10%', marginLeft:'45%', marginTop:'10%', position: 'fixed'}}/> : 
       listaProdutos.map((item,index) => ( 

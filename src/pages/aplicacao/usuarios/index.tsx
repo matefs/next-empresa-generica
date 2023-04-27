a4cfe81ba739bf94ead4ff4c1c15ab8c32f52f50
Mateus Schverz <mate_fs@hotmail.com>
2023-04-27 09:15:37 -0300

    add: linha de organização para produtso
    fix: ordenação do ultimo id para o primeiro e com paginacao limite de 20 usuarios

--- src/pages/aplicacao/usuarios/index.tsx
+++ src/pages/aplicacao/usuarios/index.tsx
@@ -1,24 +1,34 @@
 import React, { useState, useEffect} from 'react'
 import axios from 'axios'
-import {CircularProgress, Box, Typography } from "@mui/material";
+import {CircularProgress, Box, Typography, Avatar, Card, CardContent} from "@mui/material";
+import Header from '../../../componentes/Header';
 
 
 export default function Index(){
-    const [ carregandoBooleano, setCarregandoBooleano ] = useState(false)
+    const [ carregandoBooleano, setCarregandoBooleano ] = useState(true)
+    const [ listaUsuarios, setListaUsuarios ] = useState([])
 
     function carregarVariosUsuarios(){
-        axios.get('https://generic-api-backend.mateusschverz.repl.co/usuarios')
+        axios.get('https://generic-api-backend.mateusschverz.repl.co/usuarios?_limit=20&_sort=id&_order=desc')
         .then(resposta => {
-            Object.keys(resposta.data).length > 1 ? setCarregandoBooleano(true) : alert('Houve um erro ao carregar os usuários, tente mais tarde.')
+            Object.keys(resposta.data).length > 1 ? (function(){
+                setListaUsuarios(resposta.data)
+                setCarregandoBooleano(false)
+            })()
+              : alert('Houve um erro ao carregar os usuários, tente mais tarde.')
         })
     }
+
+
 
     useEffect(() => {
         carregarVariosUsuarios()
     },[])
 
     return( 
-        <>
+        <> 
+
+          <Typography variant='h3' sx={{textAlign:"center"}}>Lista de usuários</Typography>
 <Box
         sx={{
           bgcolor: "background.paper",
@@ -27,14 +37,16 @@ export default function Index(){
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          flexWrap: "wrap",
+          flexDirection: 'row',
+          flexWrap: "wrap", 
           gap: { sm: 4, md: 5, lg: 9 },
         }}
       >
 
-          <Typography variant='h3'>Lista de usuários</Typography>
 
-        { carregandoBooleano == false ? 
+ 
+
+       { carregandoBooleano == true ? 
                   <CircularProgress
                   style={{
                       width: "10%",
@@ -43,9 +55,32 @@ export default function Index(){
                       position: "fixed",
                     }}
                     />
-                    : "Carregou" }
+                    : listaUsuarios.map((usuario) => {
+                        return(
+                            <Card
+                                key={usuario.id}
+                                sx={{ width: 300, maxWidth: 745, cursor: `pointer`, marginTop: 2 }} 
+                                >
+                                <Avatar
+                                    sx={{ height: 260, maxHeight: 700, width: '100%'}} 
+                                />
+
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                    {usuario.nome}
+                                    </Typography>
+
+                                    <Typography variant="body2" color="text.secondary">Endereco: {usuario.cidade}/{usuario.estado} </Typography>
+                                    <Typography variant='body2' color='text.secondary'> E-mail: {usuario.email} </Typography>
+                                    <Typography variant='body2' color='text.secondary'> CPF: {usuario.cpf} </Typography>
+                                    
+                                </CardContent>
+                            </Card>
+                            )
+                        })}
 
 </Box>
+ 
         </>
     )
 }
